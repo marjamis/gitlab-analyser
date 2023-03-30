7da6cf64d5b4ca807a62f587fb69e35a54cc1a61
marjamis <10202014+marjamis@users.noreply.github.com>
2023-03-30 18:06:12 +1100

    refactor: :recycle: Moved around code to have the functions to be separated from the queries, as much as possible

--- engine/get.py
+++ engine/get.py
@@ -1,14 +1,16 @@
-from engine.configuration import Branch, Commit, Group, Project, PipelineSchedule, User
-from gql import gql, Client
-from gql.transport.aiohttp import AIOHTTPTransport
 import pickle
 import os
-from typing import List
+from typing import List, Dict, Any
+
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+
+from engine.configuration import Branch, Commit, Group, Project, PipelineSchedule, User
 
 # TODO Improve opening off these, such as with a context manager and proper error checking
-groups_gq = gql(open("queries/groups.gql", "r").read())
-projects_gq = gql(open("queries/projects.gql", "r").read())
-branches_gq = gql(open("queries/branch.gql", "r").read())
+groups_and_projects_query = gql(open("queries/groups_and_projects.gql", "r").read())
+project_details_query = gql(open("queries/project_details.gql", "r").read())
+branch_details_query = gql(open("queries/branch_details.gql", "r").read())
 
 
 def create_gitlab_client() -> Client:
@@ -22,13 +24,15 @@ def create_gitlab_client() -> Client:
     return Client(transport=transport, fetch_schema_from_transport=True)
 
 
-def get_groups(client: Client) -> List[Group]:
-    groups = []
-
-    groups_result = client.execute(
-        groups_gq,
-        variable_values={},
+def make_query(client: Client, query: gql, variables: dict = None) -> Dict[str, Any]:
+    return client.execute(
+        query,
+        variable_values=variables,
     )
+
+
+def create_groups_and_projects(groups_result: Dict[str, Any]) -> List[Group]:
+    groups = []
 
     for group in groups_result["groups"]["nodes"]:
         new_group = Group(
@@ -55,66 +59,48 @@ def get_groups(client: Client) -> List[Group]:
     return groups
 
 
-def get_branch_details(client: Client, project: Project, branch_name: str) -> Branch:
-    new_branch = Branch(name=branch_name)
+def create_branch(branch_name: str) -> Branch:
+    return Branch(name=branch_name)
 
-    last_commit_result = client.execute(
-        branches_gq,
-        variable_values={
-            "project": f"{project.full_path}",
-            "branch": branch_name,
-        },
-    )
 
+def create_branch_details(branch: Branch, branch_details: Dict[str, Any]):
     try:
-        ref = last_commit_result["project"]["repository"]["tree"]["lastCommit"]
-        new_branch.set_last_commit(
+        ref = branch_details["project"]["repository"]["tree"]["lastCommit"]
+        branch.set_last_commit(
             Commit(
                 title=ref["title"],
                 authored_date=ref["authoredDate"],
                 author_name=ref["authorName"],
             )
         )
-    except TypeError as te:
-        print(f"There is a TypeError of: {te}")
-
-    return new_branch
+    except TypeError as type_error:
+        print(f"There is a TypeError of: {type_error}")
 
 
-def get_pipeline_schedule_details(pipeline_schedule) -> PipelineSchedule:
+def create_pipeline_schedule(pipeline_schedule: Dict[str, Any]) -> PipelineSchedule:
     node = pipeline_schedule["node"]
     owner = pipeline_schedule["node"]["owner"]
+
     return PipelineSchedule(
         id=node["id"],
         description=node["description"],
         active=node["active"],
         next_run_at=node["nextRunAt"],
         owner=User(
-                id=owner["id"],
-                username=owner["username"],
-                public_email=owner["publicEmail"],
-                state=owner["state"],
-            )
-        )
+            id=owner["id"],
+            username=owner["username"],
+            public_email=owner["publicEmail"],
+            state=owner["state"],
+        ),
+    )
 
 
-def get_branches(client: Client, groups: List[Group]) -> List[Group]:
-    for group in groups:
-        for project in group.projects:
-            branches_list_result = client.execute(
-                projects_gq,
-                variable_values={
-                    "fullPath": f"{project.full_path}",
-                },
-            )
+def create_project_details(project: Project, project_details: Dict[str, Any]) -> None:
+    for branch_name in project_details["project"]["repository"]["branchNames"]:
+        project.add_branch(create_branch(branch_name))
 
-            for branch_name in branches_list_result["project"]["repository"]["branchNames"]:
-                project.add_branch(get_branch_details(client, project, branch_name))
-
-            for pipeline_schedule in branches_list_result["project"]["pipelineSchedules"]["edges"]:
-                project.add_pipeline_schedule(get_pipeline_schedule_details(pipeline_schedule))
-
-    return groups
+    for pipeline_schedule in project_details["project"]["pipelineSchedules"]["edges"]:
+        project.add_pipeline_schedule(create_pipeline_schedule(pipeline_schedule))
 
 
 def pickle_and_save(output_pickle_filename: str, groups: List[Group]) -> None:
@@ -122,10 +108,58 @@ def pickle_and_save(output_pickle_filename: str, groups: List[Group]) -> None:
     pickle.dump(groups, file)
 
 
+# TODO this shouldnt be needed by for some reason is
+def get_value(results: Dict[str, Any], key: str) -> Any:
+    return results["groups"]["pageInfo"][key]
+
+
 def get_data(output_pickle_filename: str) -> None:
     client = create_gitlab_client()
 
-    groups = get_groups(client)
-    groups = get_branches(client, groups)
+    # TODO deal with pagination in each query and make it more seamless than here
+    groups = []
+    has_next_page = True
+    after = ""
+    while has_next_page:
+        results = make_query(
+            client,
+            groups_and_projects_query,
+            variables={
+                # Temporary to test pagination
+                # "numOfResults": 1,
+                "after": after,
+            },
+        )
+        groups += create_groups_and_projects(results)
+        if get_value(results, "hasNextPage"):
+            after = get_value(results, "endCursor")
+        else:
+            has_next_page = False
+
+    for group in groups:
+        for project in group.projects:
+            project_details = make_query(
+                client,
+                project_details_query,
+                variables={
+                    "projectFullPath": f"{project.full_path}",
+                },
+            )
+
+            create_project_details(project, project_details)
+
+    for group in groups:
+        for project in group.projects:
+            for branch in project.get_branches():
+                branch_details = make_query(
+                    client,
+                    branch_details_query,
+                    variables={
+                        "projectFullPath": f"{project.full_path}",
+                        "branchName": branch.name,
+                    },
+                )
+
+                create_branch_details(branch, branch_details)
 
     pickle_and_save(output_pickle_filename, groups)

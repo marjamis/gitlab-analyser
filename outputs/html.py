f880466367002bf06c92ab580b65b8e395bb053a
marjamis <10202014+marjamis@users.noreply.github.com>
2023-10-29 15:44:03 +1100

    refactor: :recycle: Cleaned up the html process to be a bit more generic

--- outputs/html.py
+++ outputs/html.py
@@ -3,13 +3,14 @@ from nbconvert import HTMLExporter
 from nbconvert.preprocessors.execute import ExecutePreprocessor
 
 
-def html_report(jupyter_notebook_location: str):
-    """Generates a html file based on the provided Jupyter notebook.
+def html_report(jupyter_notebook_location: str, output_html_location: str):
+    """Generates a html file based on the provided Jupyter notebook and saves it.
 
     Args:
         jupyter_notebook_location (str): "File location of the Jupyter notebook to be used"
+        output_html_location (str): "File location to save the html output"
     """
-    with open(jupyter_notebook_location) as file:
+    with open(jupyter_notebook_location, "r", encoding="utf-8") as file:
         notebook = nbformat.read(file, as_version=4)
 
     processor = ExecutePreprocessor(timeout=600, kernel_name="python3")
@@ -19,7 +20,7 @@ def html_report(jupyter_notebook_location: str):
     html_exporter.exclude_input = True
     html_exporter.exclude_output_prompt = True
 
-    (body, resources) = html_exporter.from_notebook_node(notebook)
+    (body, _) = html_exporter.from_notebook_node(notebook)
 
-    with open("./data/output.html", "w", encoding="utf-8") as file:
+    with open(output_html_location, "w", encoding="utf-8") as file:
         file.write(body)
